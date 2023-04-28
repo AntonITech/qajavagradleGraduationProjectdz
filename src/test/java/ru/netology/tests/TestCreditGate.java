f5ab9be8ddb99877337fc8454b0c95027cdf3f1e
AntonI <avmiass@bk.ru>
2023-04-28 19:34:04 +0300

    add docker

--- src/test/java/ru/netology/tests/TestCreditGate.java
+++ src/test/java/ru/netology/tests/TestCreditGate.java
@@ -1,3 +1,4 @@
 package ru.netology.tests;
 public class TestCreditGate {
+
 }
--- src/test/java/ru/netology/tests/TestPaymentGate.java
+++ src/test/java/ru/netology/tests/TestPaymentGate.java
@@ -1,3 +1,13 @@
 package ru.netology.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import ru.netology.data.DashboardPage;
+
 public class TestPaymentGate {
+    private DashboardPage dashboardPage;
+
+    @BeforeEach
+    void setUpPage() {
+        dashboardPage = new DashboardPage();
+    }
 }
